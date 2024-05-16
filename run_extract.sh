4bbf7d0e880c36317c20d0bb4a90b2dda26c18f3
gabitza-tech <gabriel.pirlogeanu@gmail.com>
2024-05-16 11:11:35 +0200

    bash to extract embs

---
+++ run_extract.sh
@@ -0,0 +1,14 @@
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_3s_dur_1.5_ovl_0.75_support.json voxceleb1_3s_dur_1.5_ovl_0.75_metasr
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_3s_dur_1.5_ovl_0.75_query.json voxceleb1_3s_dur_1.5_ovl_0.75_metasr
+
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_1.5_ovl_0.75_support.json voxceleb1_5s_dur_1.5_ovl_0.75_metasr
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_1.5_ovl_0.75_query.json voxceleb1_5s_dur_1.5_ovl_0.75_metasr
+
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_3s_dur_1_ovl_0.5_support.json voxceleb1_3s_dur_1_ovl_0.5_metasr
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_3s_dur_1_ovl_0.5_query.json voxceleb1_3s_dur_1_ovl_0.5_metasr
+
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_1_ovl_0.5_support.json voxceleb1_5s_dur_1_ovl_0.5_metasr
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_1_ovl_0.5_query.json voxceleb1_5s_dur_1_ovl_0.5_metasr
+
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_3_ovl_0.9_support.json voxceleb1_5s_dur_3_ovl_0.9_metasr
+python3 test_features.py ../NeMo/cvn_sr/manifests/voxceleb1_5s_dur_3_ovl_0.9_query.json voxceleb1_5s_dur_3_ovl_0.9_metasr
